eeca5e65d9c60efd83e221d8d3eaa10f04b0a140
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-05-11 17:45:53 +0530

    updated

--- day14/src/linearalgebra/utils/census_mat_transpose.py
+++ day14/src/linearalgebra/utils/census_mat_transpose.py
@@ -8,6 +8,13 @@ def census_mat_transpose():
     census_data = pd.read_csv(Config.census_path)
     #create subset of census data with state, population
     df= census_data[['state','male_population','female_population']]
+    #set state as index
+    df.set_index('state', inplace=True)
+    #transpose the dataframe
+    df = df.transpose()
+    #group by state and sum the population
+    df = df.T.groupby(level=0).sum()
+    df = df.transpose()
     print(df.head())
 
 if __name__ == "__main__":
