3c3ce364bbc8fc79ea7abb9f775fef3e161ffb0c
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-05-13 16:04:57 +0530

    updated

--- day14/src/linearalgebra/utils/cyber_svd.py
+++ day14/src/linearalgebra/utils/cyber_svd.py
@@ -1,6 +1,7 @@
 #create svd for cyber security text file
 from linearalgebra.configurations.conf import Config
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import TruncatedSVD
 def calculate_svd(file_path):
     #open text file
     with(open(file_path, 'r',encoding='utf-8')) as f:
@@ -8,7 +9,7 @@ def calculate_svd(file_path):
     #print(text)
     #split text into smaller documents
     documents=[p.strip() for p in text.split('\n') if p.strip()]
-    #print(documents)
+    print(documents)
     #length of total policies
     print(f"Total policies: {len(documents)}") 
     # apply TF and ITF vectorization to the documents
@@ -16,8 +17,16 @@ def calculate_svd(file_path):
         stop_words="english"    
     )
     X = vectorizer.fit_transform(documents)
-    print(f"TF-IDF matrix  {X}")
-    print(f"TF-IDF matrix shape: {X.shape}")
+    #print(f"TF-IDF matrix  {X}")
+    #print(f"TF-IDF matrix shape: {X.shape}")
+    # apply SVD to the TF-IDF matrix
+    svd = TruncatedSVD(n_components=2)
+    embeddings = svd.fit_transform(X)
+    print(f"SVD matrix: {embeddings}")
+    print(f"SVD matrix shape: {embeddings.shape}")
+
+   
+
 
 
 
