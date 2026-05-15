c2383ea3c8e5540ae9dab23283290177e8f3b9e1
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-05-15 17:36:44 +0530

    updated

--- day14/src/linearalgebra/utils/parallel.py
+++ day14/src/linearalgebra/utils/parallel.py
@@ -9,26 +9,35 @@ def sequential_tax_calculator(product_path):
     #read products.csv
     products = pd.read_csv(product_path)
     start_time = time.time()
-    products['tax'] = products['cost'] * 0.1
+    #convert cost column to array
+    cost_array = products['cost'].values
+    tax_array = []
+    for i in range(len(cost_array)):
+        tax = cost_array[i] * 0.1
+        tax_array.append(tax)
     end_time = time.time()
     print(f"Time taken for sequential tax calculation: {end_time - start_time} seconds")
-    return products
+    return tax_array
+def calculate_tax(cost):
+    return cost * 0.1
 def parallel_tax_calculator(product_path):
-    #read products.csv
-    #with pool of 8 processes, calculate tax for each product in products.csv and add it as a new column to the dataframe
     products = pd.read_csv(product_path)
+
+    cost_array = products["cost"].values
     start_time = time.time()
-    with Pool(8) as p:
-        products['tax'] = products['cost'] * 0.1
+    with Pool() as p:
+        tax_array = p.map(calculate_tax, cost_array)
     end_time = time.time()
     print(f"Time taken for parallel tax calculation: {end_time - start_time} seconds")
-    return products
+    return tax_array
+
+    
 
 
 if __name__ == "__main__":
     #read products.csv
     product_path=Config.product_path
-    products = sequential_tax_calculator(product_path)
-    print(products)
-    products = parallel_tax_calculator(product_path)
-    print(products)
+    tax = sequential_tax_calculator(product_path)
+    tax=print(tax)
+    parallel_tax_calculator(product_path)
+ 
