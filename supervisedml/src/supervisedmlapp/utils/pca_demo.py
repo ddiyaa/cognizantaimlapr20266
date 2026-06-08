7839a92d6a035663118f5e62b8a8d94ed1768581
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-06-08 15:44:17 +0530

    updated

---
+++ supervisedml/src/supervisedmlapp/utils/pca_demo.py
@@ -0,0 +1,34 @@
+#create pca for tshirt data in csv file
+#consider target columns QaulityClass, Rest all inputs
+
+import pandas as pd
+from sklearn.decomposition import PCA   
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt 
+from supervisedmlapp.configurations.conf import T_SHIRT_FILE_PATH
+from sklearn.preprocessing import LabelEncoder
+def pca_analysis():
+    # Load the dataset
+    data = pd.read_csv(T_SHIRT_FILE_PATH)
+
+    #apply label encoding to the target variable
+   
+    label_encoder = LabelEncoder()
+    y = label_encoder.fit_transform(data['QualityClass'])
+
+    #apply one hot encoding to color,fabric,brand
+    data = pd.get_dummies(data, columns=['Color', 'Fabric', 'Brand'])   
+  
+   
+    # Separate features and target variable
+    X = data.drop('QualityClass', axis=1)  # Features
+    
+    #print x and y
+    print("Features (X):")
+    print(X.head())
+    print("\nTarget variable (y):")
+    print(y[:5])
+
+
+if __name__ == "__main__":
+    pca_analysis()
