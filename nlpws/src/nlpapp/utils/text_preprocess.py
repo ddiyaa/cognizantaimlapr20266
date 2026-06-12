ba24c5e2734cba71af5a6cf47cdde9ceb92313a6
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-06-12 12:17:41 +0530

    updated

---
+++ nlpws/src/nlpapp/utils/text_preprocess.py
@@ -0,0 +1,38 @@
+#scrape the site in .env
+
+import os
+import re
+from dotenv import load_dotenv
+import nltk
+from nltk.corpus import stopwords
+from bs4 import BeautifulSoup
+import requests
+
+env_path = os.path.join(os.path.dirname(__file__), '..', '.env')
+load_dotenv(dotenv_path=env_path)
+
+scrape_url = os.getenv("scrape_url")
+
+#create the headers 
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+}
+
+#read text from the url
+def read_text_from_url(url):
+      
+        response = requests.get(url, headers=headers,timeout=20)
+        if response.status_code == 200:
+            return response.text
+        else:
+            raise Exception(f"Failed to fetch data from {url}. Status code: {response.status_code}")
+    
+
+
+if __name__ == "__main__":
+    try:
+        text = read_text_from_url(scrape_url)
+        print(text[:500])  # Print the first 500 characters of the fetched text
+    except Exception as e:
+        print(str(e))
+    
